58b0350ef65efc3095b44fa594c6515693bfca2c
Gaurav <42097215+CodebyGaurav@users.noreply.github.com>
2022-01-07 19:17:30 +0530

    Add files via upload

---
+++ limits-service/src/main/java/com/codebygaurav/limits/controller/LimitsConfigurationController.java
@@ -0,0 +1,19 @@
+package com.codebygaurav.limits.controller;
+
+
+import com.codebygaurav.limits.bean.LimitsConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @Autowired
+    private  LimitsConfiguration limitsConfiguration;
+
+    @GetMapping("limits")
+    public LimitsConfiguration returnLimits(){
+        return new LimitsConfiguration(limitsConfiguration.getMaximum(),limitsConfiguration.getMinimum());
+    }
+}
